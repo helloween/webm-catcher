8305989e5feced1e9dbf087e214ee6eb8ddf103c
Yury Hurski <hi1sailor@gmail.com>
2016-05-13 21:23:01 +0300

    Base index page, routes;

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 require('dotenv').config();
+
+var app     = require('koa')();
+var views   = require('koa-views');
+var favicon = require('koa-favicon');
+var router  = require('./routes');
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(views(__dirname + '/build'));
+app.use(router.routes());
+
+app.listen(process.env.APP_PORT);
